b763254f648b60cd8b733b19117f10addbba723b
PekoMan2 <peterkuppe2@gmail.com>
2025-04-12 16:01:47 +0200

    fix: fill.sh comment

--- fill.sh
+++ fill.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script to fill the database with test data, used for debugging purposes
+
 curl -X POST http://localhost:3000/events -H "Content-Type: application/json" -d '{"name": "Ahoj1", "description": "This is a test event", "from": "2025-04-10T10:00:00Z", "to": "2025-04-10T12:00:00Z"}'
 
 curl -X POST http://localhost:3000/events -H "Content-Type: application/json" -d '{"name": "joha", "description": "This is a test event", "from": "2025-04-10T10:00:00Z", "to": "2025-04-10T12:00:00Z"}'
